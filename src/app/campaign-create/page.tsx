d8bea85486bf9217646417bfff8236117026a152
MedDhiaChaouachi <mohameddhiach+1@gmail.com>
2025-03-26 12:54:57 +0100

    create compaign-create router

---
+++ src/app/campaign-create/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function CampaignCreatePage() {
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <h1 className="text-2xl font-bold">Hello World</h1>
+    </div>
+  );
+}
